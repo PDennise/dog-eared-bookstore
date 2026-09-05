77a70c1c3fab8368a82f4e8b44fa26dc1c6a5683
Deniz Pandar <denizpandar@gmail.com>
2026-09-05 16:39:33 +0100

    chore: create Django apps

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "rest_framework",
+
+    "accounts",
+    "home",
+    "products",
+    "cart",
+    "orders",
+    "ai",
 ]
 
 MIDDLEWARE = [
