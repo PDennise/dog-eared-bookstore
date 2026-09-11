38fd3d4e1cc4b724775b05a888a9646e9874fc58
Deniz Pandar <denizpandar@gmail.com>
2026-09-11 21:34:19 +0100

    Add product API viewsets

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Book, Category
+from .serializers import BookSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Provide the standard CRUD API actions for categories."""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """Provide the standard CRUD API actions for books."""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
