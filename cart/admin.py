f51823c744e78b83f0ffad5bd51cb9419fd595c6
Deniz Pandar <denizpandar@gmail.com>
2026-09-19 16:03:02 +0100

    Register Cart and CartItem in Django admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    """Show cart items on the cart page itself."""
+
+    model = CartItem
+    extra = 1
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """Customize the Django admin interface for carts."""
+
+    list_display = ("id", "user", "created_at", "updated_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username",)
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    """Customize the Django admin interface for cart items."""
+
+    list_display = ("id", "cart", "book", "quantity", "added_at")
+    list_filter = ("added_at",)
+    search_fields = ("book__name",)
