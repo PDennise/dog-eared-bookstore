bfad0e9904f55e49aad0c605db65e070736a91e7
Deniz Pandar <denizpandar@gmail.com>
2026-09-20 01:06:37 +0100

    Add initial order  models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.conf import settings
+from products.models import Book
 
-# Create your models here.
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        PAID = "paid", "Paid"
+        CANCELLED = "cancelled", "Cancelled"
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f" Order #{self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+    book = models.ForeignKey(
+        Book, 
+        on_delete=models.PROTECT,
+        related_name="order_items",
+    )
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return f"{self.book.name} x {self.quantity}"
+    
